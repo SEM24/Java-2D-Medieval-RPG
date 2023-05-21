c316092d4df8d9ebccb9593fc80aabdd0900bb88
Samer Khomsi Kak <samerx777222@gmail.com>
2023-05-22 00:22:53 +0200

    Added new map to game (Boss room).

--- src/main/java/com/khomsi/game/main/logic/EventHandler.java
+++ src/main/java/com/khomsi/game/main/logic/EventHandler.java
@@ -18,26 +18,17 @@ public class EventHandler {
         this.gameManager = gameManager;
         eventRect = new EventRect[gameManager.maxMap][GameManager.maxWorldCol][GameManager.maxWorldRow];
         eventMaster = new Entity(gameManager);
-        int col = 0;
-        int row = 0;
-        int map = 0;
 
-        while (map < gameManager.maxMap && col < GameManager.maxWorldCol &&
-                row < GameManager.maxWorldRow) {
-            eventRect[map][col][row] = new EventRect();
-            eventRect[map][col][row].x = 23;
-            eventRect[map][col][row].y = 23;
-            eventRect[map][col][row].width = 2;
-            eventRect[map][col][row].height = 2;
-            eventRect[map][col][row].eventRectDefaultX = eventRect[map][col][row].x;
-            eventRect[map][col][row].eventRectDefaultY = eventRect[map][col][row].y;
-            col++;
-            if (col == GameManager.maxWorldCol) {
-                col = 0;
-                row++;
-                if (row == GameManager.maxWorldRow) {
-                    row = 0;
-                    map++;
+        for (int map = 0; map < gameManager.maxMap; map++) {
+            for (int col = 0; col < GameManager.maxWorldCol; col++) {
+                for (int row = 0; row < GameManager.maxWorldRow; row++) {
+                    eventRect[map][col][row] = new EventRect();
+                    eventRect[map][col][row].x = 23;
+                    eventRect[map][col][row].y = 23;
+                    eventRect[map][col][row].width = 2;
+                    eventRect[map][col][row].height = 2;
+                    eventRect[map][col][row].eventRectDefaultX = eventRect[map][col][row].x;
+                    eventRect[map][col][row].eventRectDefaultY = eventRect[map][col][row].y;
                 }
             }
         }
@@ -55,38 +46,96 @@ public class EventHandler {
         int xDistance = Math.abs(gameManager.player.worldX - previousEventX);
         int yDistance = Math.abs(gameManager.player.worldY - previousEventY);
         int distance = Math.max(xDistance, yDistance);
+
         if (distance > GameManager.TILE_SIZE) {
             canTouchEvent = true;
         }
-        if (canTouchEvent) {
-            if (interact(0, 27, 20, "any")) damagePit(GameManager.DIALOGUE_STATE);
-            else if (interact(0, 20, 35, "down")
-                    || interact(0, 19, 35, "down")
-                    || interact(0, 18, 35, "down")) healingPool(GameManager.DIALOGUE_STATE);
-                //Col and row of enter location is the place where
-                //the player will be spawned on new map
-            else if (interact(0, 18, 25, "any")) { //Seller
-                changeLocation(1, 20, 22, GameManager.INDOOR);
-                gameManager.playSE(4);
-            }
-            //back to main map
-            else if (interact(1, 20, 23, "any")) {
-                changeLocation(0, 18, 26, GameManager.OUTSIDE);
-                gameManager.playSE(5);
 
+        if (canTouchEvent) {
+            if (interact(0, 27, 20, "any")) {
+                damagePit(GameManager.DIALOGUE_STATE);
+            } else {
+                handleInteractions();
             }
-            //Dungeon
-            else if (interact(1, 25, 15, "up") ||
-                    interact(1, 24, 15, "up")) {
-                changeLocation(2, 16, 39, GameManager.DUNGEON);
-                gameManager.playSE(4);
-            } else if (interact(2, 15, 39, "any")) {
-                changeLocation(1, 25, 15, GameManager.INDOOR);
-                gameManager.playSE(5);
-            }
-            //gameManager.npcList[1][0], 1 - the map number, 0 - number of npc on this map
-            else if (interact(1, 20, 18, "up")) speak(gameManager.npcList[1][0]);
         }
+    }
+
+    private void handleInteractions() {
+        int currentMap = gameManager.currentMap;
+        switch (currentMap) {
+            case 0 -> { // Main Map
+                if (interact(0, 18, 35, "down") || interact(0, 19, 35, "down")
+                        || interact(0, 20, 35, "down")) {
+                    healingPool(GameManager.DIALOGUE_STATE);
+                } else if (interact(0, 18, 25, "any")) {
+                    changeLocation(1, 20, 22, GameManager.INDOOR);
+                    gameManager.playSE(4);
+                }
+            }
+            case 1 -> { // Seller House
+                //back to main map from seller
+                if (interact(1, 20, 23, "any")) {
+                    changeLocation(0, 18, 26, GameManager.OUTSIDE);
+                    gameManager.playSE(5);
+                }
+                //Enter Dungeon in seller house
+                else if (interact(1, 25, 15, "up") || interact(1, 24, 15, "up")) {
+                    changeLocation(2, 16, 39, GameManager.DUNGEON);
+                    gameManager.playSE(4);
+                }
+                //Talk to seller in house
+                else if (interact(1, 20, 18, "up")) {
+                    speak(gameManager.npcList[1][0]);
+                }
+            }
+            case 2 -> { // Dungeon
+                //Back to seller house from dungeon
+                if (interact(2, 15, 39, "any")) {
+                    changeLocation(1, 25, 15, GameManager.INDOOR);
+                    gameManager.playSE(5);
+                }
+                //Enter the boss dungeon
+                else if (interact(2, 9, 14, "any")) {
+                    changeLocation(3, 25, 1, GameManager.BOSS_DUNGEON);
+                    gameManager.playSE(4);
+                }
+            }
+            case 3 -> { // Boss Dungeon
+                //Exit the boss dungeon
+                if (interact(3, 26, 1, "any")) {
+                    changeLocation(2, 9, 15, GameManager.BOSS_DUNGEON);
+                    gameManager.playSE(4);
+                }
+            }
+        }
+    }
+
+    public boolean interact(int map, int col, int row, String direction) {
+        boolean interact = false;
+
+        if (map == gameManager.currentMap) {
+
+            gameManager.player.solidArea.x = gameManager.player.worldX + gameManager.player.solidArea.x;
+            gameManager.player.solidArea.y = gameManager.player.worldY + gameManager.player.solidArea.y;
+
+            eventRect[map][col][row].x = col * GameManager.TILE_SIZE + eventRect[map][col][row].x;
+            eventRect[map][col][row].y = row * GameManager.TILE_SIZE + eventRect[map][col][row].y;
+            //if player's solidArea is colliding with event's solidArea, event can be played only 1 time
+            if (gameManager.player.solidArea.intersects(eventRect[map][col][row]) &&
+                    !eventRect[map][col][row].eventDone) {
+                if (gameManager.player.direction.contentEquals(direction) ||
+                        direction.contentEquals("any")) {
+                    interact = true;
+                    previousEventX = gameManager.player.worldX;
+                    previousEventY = gameManager.player.worldY;
+                }
+            }
+            gameManager.player.solidArea.x = gameManager.player.solidAreaDefaultX;
+            gameManager.player.solidArea.y = gameManager.player.solidAreaDefaultY;
+            eventRect[map][col][row].x = eventRect[map][col][row].eventRectDefaultX;
+            eventRect[map][col][row].y = eventRect[map][col][row].eventRectDefaultY;
+        }
+        return interact;
     }
 
     private void speak(Entity entity) {
@@ -143,34 +192,5 @@ public class EventHandler {
         gameManager.player.worldX = GameManager.TILE_SIZE * 24;
         gameManager.player.worldY = GameManager.TILE_SIZE * 11;
         gameManager.player.fallIntoPit = false;
-    }
-
-    public boolean interact(int map, int col, int row, String direction) {
-        boolean interact = false;
-
-        if (map == gameManager.currentMap) {
-
-
-            gameManager.player.solidArea.x = gameManager.player.worldX + gameManager.player.solidArea.x;
-            gameManager.player.solidArea.y = gameManager.player.worldY + gameManager.player.solidArea.y;
-
-            eventRect[map][col][row].x = col * GameManager.TILE_SIZE + eventRect[map][col][row].x;
-            eventRect[map][col][row].y = row * GameManager.TILE_SIZE + eventRect[map][col][row].y;
-            //if player's solidArea is colliding with event's solidArea, event can be played only 1 time
-            if (gameManager.player.solidArea.intersects(eventRect[map][col][row]) &&
-                    !eventRect[map][col][row].eventDone) {
-                if (gameManager.player.direction.contentEquals(direction) ||
-                        direction.contentEquals("any")) {
-                    interact = true;
-                    previousEventX = gameManager.player.worldX;
-                    previousEventY = gameManager.player.worldY;
-                }
-            }
-            gameManager.player.solidArea.x = gameManager.player.solidAreaDefaultX;
-            gameManager.player.solidArea.y = gameManager.player.solidAreaDefaultY;
-            eventRect[map][col][row].x = eventRect[map][col][row].eventRectDefaultX;
-            eventRect[map][col][row].y = eventRect[map][col][row].eventRectDefaultY;
-        }
-        return interact;
     }
 }
